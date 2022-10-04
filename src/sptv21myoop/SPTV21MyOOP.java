2fb7214ca2a1df38f94349ae1abf71c3b11fe36e
Anatoli <anatoli.ivanov@ivkhk.ee>
2022-10-04 14:14:52 +0300

    commit

--- src/sptv21myoop/SPTV21MyOOP.java
+++ src/sptv21myoop/SPTV21MyOOP.java
@@ -15,7 +15,7 @@ public class SPTV21MyOOP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Book book = new Book();
+        
     }
     
 }
