8f2dbbafae114bd9780d58f3cc68bbbf67175f35
Anatoli <anatoli.ivanov@ivkhk.ee>
2022-10-04 15:19:10 +0300

    New commit

--- src/sptv21myoop/SPTV21MyOOP.java
+++ src/sptv21myoop/SPTV21MyOOP.java
@@ -5,6 +5,9 @@
  */
 package sptv21myoop;
 
+import myclasses.Author;
+import myclasses.Book;
+
 /**
  *
  * @author Anatoli
@@ -15,7 +18,15 @@ public class SPTV21MyOOP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Author author1 = new Author();
+        author1.setFirstname("Aleksandr");
+        author1.setLastname("Pushkin");
+        Book book1 = new Book();
+        book1.setCaption("Jevgeny Onegin");
+        Author[] authors = new Author[1];
+        authors[0] = author1;
+        book1.setAuthors(authors);
+        System.out.println(book1.toString());
     }
     
 }
